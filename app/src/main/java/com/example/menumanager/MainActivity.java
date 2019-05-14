37ea296abce589cf5cde27d104230160581e578a
Monica <monica.riera@bts.tech>
2019-05-14 13:05:40 +0200

    Implemented main and contextual menus

--- app/src/main/java/com/example/menumanager/MainActivity.java
+++ app/src/main/java/com/example/menumanager/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.menumanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.activity_main__tv__title);
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_main__item__copy) {
+            Toast.makeText(this, "Copy", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.menu_main__item__cut) {
+            Toast.makeText(this, "Cut", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.menu_main__item__paste) {
+            Toast.makeText(this, "Paste", Toast.LENGTH_SHORT).show();
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.floating_menu, menu);
     }
 }
